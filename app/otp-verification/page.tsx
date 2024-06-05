741f52c02aba0a7086d77675fd9d21f91f6a2ba7
Swetank <swetank199@gmail.com>
2024-06-05 19:27:09 +0530

    css fixes

--- app/otp-verification/page.tsx
+++ app/otp-verification/page.tsx
@@ -11,44 +11,52 @@ import {
 export default function otpVerification() {
 	return (
 		<div className='flex flex-col h-dvh justify-center items-center font'>
-			<div className='flex flex-col h-[50dvh] justify-end max-w-64'>
-				<div className=' text-xs font-serif text-primary pb-1'>
+			<div className='flex flex-col h-[50dvh] justify-end min-w-72'>
+				<div className=' text-xs font-serif text-primary pb-1 '>
 					MEMBERSHIP APPLICATION
 				</div>
-				<div className='font-gilroy-light text-2xl '>enter the OTP sent to</div>
-				<div className='font-gilroy-light text-2xl pb-4'>number</div>
+				<div className='font-gilroy-light text-2xl text-foreground'>
+					enter the OTP sent to
+				</div>
+				<div className='font-gilroy-light text-2xl pb-4 min-w-full'>number</div>
 			</div>
 			<div className='flex flex-col bg-white h-1/2 w-full'>
 				<hr className=' h-2 mx-auto  border-0 rounded  w-full bg-secondary' />
-				<div className='flex flex-col mt-2 items-center grow pt-2  '>
-					<div className='flex flex-col max-w-72 self-center  pb-8'>
-						<InputOTP maxLength={4} className=' flex self-center'>
+				<div className='flex flex-col mt-2 items-center  pt-2  '>
+					<div className='flex flex-col max-w-72  '>
+						<InputOTP maxLength={4} className=' flex justify-center  '>
 							<InputOTPGroup>
 								<InputOTPSlot index={0} />
 								<InputOTPSlot index={1} />
 								<InputOTPSlot index={2} />
 
-								<InputOTPSlot index={3} />
+								<InputOTPSlot index={3} className='' />
 							</InputOTPGroup>
 						</InputOTP>
-						<div className='flex flex-row text-background text-xs '>
+						<div className='flex flex-row text-primary text-xs pt-4 pb-2 '>
 							<div className='flex '>didn&apos;t receive OTP?</div>
 							&nbsp;&nbsp;&nbsp;
-							<div className='underline text-xs flex-nowrap'>Resend OTP </div>
+							<div className='underline text-xs flex-nowrap text-black'>
+								Resend OTP{' '}
+							</div>
 							&nbsp;&nbsp;
-							<div className='underline text-xs flex-nowrap'>OTP on call</div>
+							<div className='underline text-xs flex-nowrap text-black'>
+								OTP on call
+							</div>
 							&nbsp;&nbsp;
 						</div>
-						<div className=' flex flex-row text-background text-xs '>
+						<div className=' flex flex-row text-primary text-xs pb-8'>
 							<p>
 								by proceeding, you are indicating that you have read and agree
-								to our <span className='underline '>terms of use</span> &amp;
-								<span className='underline '>privacy policy</span>
+								to our{' '}
+								<span className='underline text-black'>terms of use</span> &amp;
+								&nbsp;
+								<span className='underline text-black'>privacy policy</span>
 							</p>
 						</div>
 						<Link href='/otp-verification' className=''>
 							<Button
-								className='max-w-52  rounded-none bg-background text-foreground font-gilroy-extra-bold  font-light 
+								className='max-w-52  rounded-none  text-foreground font-gilroy-extra-bold  font-light 
 						 
 						'
 								variant='secondary'
--- app/utils.ts
+++ app/utils.ts
@@ -1,2 +1,77 @@
 export const delay = (ms: number) =>
 	new Promise((resolve) => setTimeout(resolve, ms));
+
+export enum FontType {
+	HEADING = 'heading',
+	CAPS = 'caps',
+	BODY = 'body',
+	SERIF_HEADING = 'serif-heading',
+}
+
+export enum FontWeights {
+	EXTRA_BOLD = 800,
+	BOLD = 700,
+	SEMI_BOLD = 600,
+	MEDIUM = 500,
+	REGULAR = 400,
+	THIN = 300,
+}
+
+export const generateTextStyle = (
+	type = FontType.BODY,
+	fontSize = 14,
+	fontWeight = FontWeights.REGULAR,
+	color = '#FFFFFF'
+) => {
+	const LINE_HEIGHT_MULTIPLIER = {
+		[FontType.HEADING]: 1.25,
+		[FontType.CAPS]: 1.25,
+		[FontType.BODY]: 1.5,
+		[FontType.SERIF_HEADING]: 1.25,
+	};
+
+	const getLetterSpacing = (
+		fType: FontType,
+		fSize: number,
+		fWeight: FontWeights
+	) => {
+		switch (fType) {
+			case FontType.HEADING:
+				switch (fWeight) {
+					case FontWeights.EXTRA_BOLD:
+						return fSize >= 44 ? 0 : 0.2;
+					case FontWeights.BOLD:
+						return fSize >= 13 ? 0.2 : 0.4;
+					case FontWeights.SEMI_BOLD:
+						return fSize >= 18 ? 0.2 : 0.4;
+					default:
+						return 0.2;
+				}
+			case FontType.CAPS:
+				if (fSize > 8) return 2;
+				return 1;
+			case FontType.BODY:
+				return 0.4;
+			case FontType.SERIF_HEADING:
+				return 0.2;
+			default:
+				return 0.4;
+		}
+	};
+
+	const letterSpacing = getLetterSpacing(type, fontSize, fontWeight);
+
+	return css`
+		font-size: ${fontSize}px;
+		font-style: normal;
+		font-weight: ${fontWeight};
+		line-height: ${Math.round(fontSize * LINE_HEIGHT_MULTIPLIER[type])}px;
+		letter-spacing: ${letterSpacing}px;
+		color: ${color ?? ''};
+		${type === FontType.CAPS
+			? css`
+					text-transform: uppercase;
+			  `
+			: ''}
+	`;
+};
